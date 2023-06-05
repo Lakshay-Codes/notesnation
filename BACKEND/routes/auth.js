5843bdc6c22153f63df5b28aad210fbc5a76e264
Lakshay <nijhawanlakshay06@gmail.com>
2023-06-05 19:54:49 +0530

    Converted get to post

--- BACKEND/routes/auth.js
+++ BACKEND/routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require('../models/User');
 
 //Create a User using: POST "/api/auth/" Doesn't require Auth
-router.get('/',(req,res)=>{
+router.post('/',(req,res)=>{
     console.log(req.body);
     const user= User(req.body);
     user.save();
