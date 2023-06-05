f38faffe7582d3f799a8d75deec13bc03221bee6
Lakshay <nijhawanlakshay06@gmail.com>
2023-06-05 19:46:43 +0530

    made our first entry in mongoDB!

--- BACKEND/db.js
+++ BACKEND/db.js
@@ -1,9 +1,9 @@
 const mongoose=require('mongoose')
-const mongoURI="mongodb://localhost:27017/";
+const mongoURI="mongodb://127.0.0.1:27017";
 mongoose.set('strictQuery', true);
 const connectToMongo=async ()=>{
     mongoose.connect(mongoURI,()=>{
-        console.log("connected to mongo! successfully!")
+        console.log("connected to mongo! successfully!");
     });
 }
 module.exports = connectToMongo;
