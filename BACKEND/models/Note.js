871e618bb9bbb45668f48cf62c0bf3431699cafc
Lakshay <nijhawanlakshay06@gmail.com>
2023-06-10 00:52:54 +0530

    Added updation functionality

--- BACKEND/models/Note.js
+++ BACKEND/models/Note.js
@@ -2,16 +2,21 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const NotesSchema = new Schema({
-  name:{
-    type: string,
+  user:{
+    //Its like a foreign key
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  title:{
+    type: String,
     required: true
   },
   description:{
-    type: string,
+    type: String,
     required: true
   },
   tag:{
-    type: string,
+    type: String,
     default:"General"
   },
   date:{
