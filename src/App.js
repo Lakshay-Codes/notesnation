1c3b5a72ca59a709446a54ae61666e2aa8594983
Lakshay <nijhawanlakshay06@gmail.com>
2023-06-16 01:32:06 +0530

    Improved Login Functionality

--- src/App.js
+++ src/App.js
@@ -9,17 +9,21 @@ import Navbar from './components/Navbar.js';
 import './App.css';
 import NoteState from "./context/notes/NoteState.js";
 import Alert from "./components/Alert.js";
+import Login from "./components/Login.js";
+import SignUp from "./components/SignUp.js";
 function App() {
   return (
     < >
     <NoteState>
       <Router>
         <Navbar />
-        <Alert message="Jai mata di"/>
+        <Alert message="Jai mata di do react from 11:30 to 1:30 daily"/>
         <div className="container">
          <Routes>
            <Route exact path='/' element={<Home />} />
            <Route exact path='/about' element={<About />} />
+           <Route exact path='/login' element={<Login />} />
+           <Route exact path='/signup' element={<SignUp />} />
          </Routes>
         </div>
        </Router>
---
+++ src/components/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+  const [credentials, setCredentials] = useState({email : "",password : ""})
+  const onChange=(e)=>{
+    setCredentials({...credentials,[e.target.name]:e.target.value})
+  }
+  const handleSubmit=async (e) => {
+    //In order to prevent unwanted page reload    
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/auth/login",{
+      method : 'POST',
+      headers : {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email : credentials.email,password : credentials.password})
+    }
+    );
+    const json = await response.json();
+    console.log(json);
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email address</label>
+          <input type="email" onChange={onChange} value={credentials.email} className="form-control" id="email" name="email" aria-describedby="emailHelp" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" onChange={onChange} value={credentials.password} name="password" className="form-control" id="password" />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
